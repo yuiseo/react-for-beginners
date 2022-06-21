aea5202804a1eb873d5c4ea06db36d77bc51c46f
유이서 <yoojh456@naver.com>
2022-06-21 23:02:55 +0900

    modify movie webpage

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import{
   BrowserRouter as Router,
   Routes,
   Route,
-  
-} from "react-router-dom"
-import Home from"./routes/Home"
-import Detail from"./routes/Detail"
+} from "react-router-dom";
+import Home from"./routes/Home";
+import Detail from"./routes/Detail";
 function App() {
   return(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Routes>
-      <Route path="/hello" element={<h1>Hello!</h1>}/>
-      <Route path="/movie/:id" element={<Detail/>}/>
-      <Route path="/" element={<Home />}/>
+      <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail/>}/>
+      <Route path={`${process.env.PUBLIC_URL}`} element={<Home />}/>
     </Routes>
   </Router>
 ) 
